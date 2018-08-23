7189448ddc6ec1b1b89031e1d9f710066bcb24bf
lbrobb <lbrobb@gmail.com>
2018-08-23 22:52:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+dodger.style.backgroundColor = "teal"
+
+dodger.style.left // "180px"
+dodger.style.bottom // "0px"
+
+dodger.style.bottom = '0px'
+
+document.addEventListener('keydown', function(e) {
+  console.log(e.which)
+})
+
 var dodger = document.getElementById('dodger')
 
 function moveDodgerLeft() {
