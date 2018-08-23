74ad02bc3f63a6066193626caecf6a4e98a97d53
lbrobb <lbrobb@gmail.com>
2018-08-23 23:00:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ function moveDodgerRight() {
   var rightNumbers = dodger.style.right.replace('px', '')
   var right = parseInt(rightNumbers, 10)
 
-  if (right < 360) {
-    dodger.style.right = `${right + 10}px`
+  if (right > 0) {
+    dodger.style.right = `${right - 10}px`
   }
 }
 
