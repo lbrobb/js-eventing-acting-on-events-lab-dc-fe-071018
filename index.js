1d2cd90ce10a4dd74916cc0b47583fe441684ac2
lbrobb <lbrobb@gmail.com>
2018-08-23 22:48:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,12 @@ dodger.style.bottom = '0px'
 document.addEventListener('keydown', function(e) {
   console.log(e.which)
 })
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+ 
+    dodger.style.left = `${left - 1}px`
+  }
+})
