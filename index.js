4bb7e1c1ab1b8576a612dfdac4ba5eceecca6266
lbrobb <lbrobb@gmail.com>
2018-08-23 22:51:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-dodger.style.backgroundColor = "pink"
-
-dodger.style.left // "180px"
-dodger.style.bottom // "0px"
-
-dodger.style.bottom = '0px'
-
-document.addEventListener('keydown', function(e) {
-  console.log(e.which)
-})
-
 var dodger = document.getElementById('dodger')
 
 function moveDodgerLeft() {
   var leftNumbers = dodger.style.left.replace('px', '')
   var left = parseInt(leftNumbers, 10)
- 
+
   if (left > 0) {
     dodger.style.left = `${left - 1}px`
   }
